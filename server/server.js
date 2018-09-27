a4aefe3db8d117b952705b49f5dd0acc41713969
Imonjirie Newton <newtonimonjirie@gmail.com>
2018-09-27 10:58:21 +0100

    Add POST /users routes and token method

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongoose').Types;
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} =  require('./models/todo');
@@ -19,9 +21,84 @@ app.post('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
-    console.log(req.body)
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // Valid id using isValid
+    if(!ObjectId.isValid(id)) return res.status(404).send();
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) return res.status(404).send();
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id
+    if(!ObjectId.isValid(id)) return res.status(404).send();
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) return res.status(404).send();
+
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectId.isValid(id)) return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed) { 
+        body.completedAt = new Date().getTime();
+    } else {
+       body.completed = false;
+       body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) return res.status(404).send();
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+//POST users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User({
+        email: body.email,
+        password: body.password
+    });
+
+    user.save().then(() => {
+       return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 });
 
 app.listen(3000, () => {
     console.log('Started on Port 3000');
 });
+
+module.exports = {app};
